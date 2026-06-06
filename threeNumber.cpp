c992d19e764b80139c93eaed967a19e16d1fee1b
Ahmed Hatem <ahmadhatem.work@gmail.com>
2026-06-06 07:49:08 +0300

    More minimalist >_>

--- threeNumber.cpp
+++ threeNumber.cpp
@@ -2,36 +2,26 @@
 
 using namespace std;
 
+
+// Fantastic Idea
+/*
+we didn'y use 3 loops and used 2 instead
+
+we know x and y, we can't just get z, if that z is in range then the answer is fulfilled
+*/
 int main(){
     int k,s;
     cin >> k >> s;
     int answer = 0;
-    for (int x =0;x<k;x++)
-    {
-        if(x > s)
-        {
-            break;
-        }
-        for(int y=0;y<k;y++)
-        {
-            if (y > s)
-            {
-                break;
+    for (int x =0;x<=k;x++){
+        for(int y=0;y<=k;y++){
+            int z = s - x - y;
+            if (0 <= z && z <= k){
+                answer++;
             }
-            
-            for (int z=0;z<k;z++)
-            {
-                if(x > s)
-                {
-                    break;
-                }
-                else if ( x+y+z == s)
-                {
-                    answer++;
-                }
             }
         }
-    }
+    
     cout << answer;
     return 0;
 }
